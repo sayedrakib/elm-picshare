e6964bdf54a7ab37cae1e38d1486626b5008e563
Rakibul Hasan Sayed <sarahasa@jyu.fi>
2018-03-01 15:04:28 +0200

    chapt 3 refactor

--- Picshare.elm
+++ Picshare.elm
@@ -4,49 +4,49 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
+type alias Model = 
+    { url : String
+    , caption : String
+    , liked: Bool 
+    }
 
 baseUrl : String 
 baseUrl = 
     "https://programming-elm.com/"
 
-initialModel : { url : String, caption : String, liked: Bool }
+initialModel : Model
 initialModel =
-    { url = baseUrl ++ "3.jpg"
-    , caption = "Evening"
-    , liked  = False
-    }
+    Model (baseUrl ++ "3.jpg") "Evening" False
 
-viewDetailedPhoto : { url : String, caption : String, liked : Bool } -> Html Msg
-viewDetailedPhoto model =
-    let 
+viewLoveButton : Model -> Html Msg
+viewLoveButton model = 
+    let
         buttonClass = 
             if model.liked then 
                 "fa-heart"
             else 
                 "fa-heart-o"
-        msg: Msg
-        msg = 
-            if model.liked then
-                Unlike
-            else
-                Like 
     in 
+    div [class "like-button"]
+        [ i 
+            [class "fa fa-2x" 
+            , class buttonClass 
+            , onClick ToggleLike 
+            ]
+            []
+        ]
+
+viewDetailedPhoto : Model -> Html Msg
+viewDetailedPhoto model =
     div [ class "detailed-photo" ]
         [ img [ src model.url ] []
         , div [ class "photo-info" ]
-            [ div [class "like-button"]
-                [ i 
-                    [class "fa fa-2x" 
-                    , class buttonClass 
-                    , onClick msg 
-                    ]
-                     []
-                ]
+            [ viewLoveButton model
             , h2 [ class "caption", align "center" ] [ text model.caption ] 
             ]
         ]
 
-view : { url : String, caption : String, liked : Bool } -> Html Msg -- Html (type) msg (type variable)
+view : Model -> Html Msg -- Html (type) msg (type variable)
 view model = 
     div []
         [ div [class "header"]
@@ -56,20 +56,15 @@ view model =
             ]
         ]
 type Msg =
-    Like | Unlike
+    ToggleLike
 
-update : Msg 
-        -> { url : String, caption : String, liked : Bool} 
-        -> { url : String, caption : String, liked : Bool } 
+update : Msg -> Model -> Model 
 update msg model = 
     case msg of 
-        Like -> 
-            { model | liked = True }
-        Unlike ->
-            { model | liked = False }
+        ToggleLike -> 
+            { model | liked = not model.liked }
 
-
-main : Program Never { url : String, caption : String, liked : Bool } Msg
+main : Program Never Model Msg
 main = 
     Html.beginnerProgram 
         { model = initialModel 
