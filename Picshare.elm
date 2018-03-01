0d509e70267133fbba8fb51e519164346eee1b96
Rakibul Hasan Sayed <sarahasa@jyu.fi>
2018-03-01 13:15:48 +0200

    chapter 2

--- Picshare.elm
+++ Picshare.elm
@@ -1,31 +1,78 @@
-module Picshare exposing (main)
+module Picshare exposing (..) 
+-- module Picshare exposing (main) 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
-baseUrl : String
+
+baseUrl : String 
 baseUrl = 
     "https://programming-elm.com/"
 
+initialModel : { url : String, caption : String, liked: Bool }
+initialModel =
+    { url = baseUrl ++ "3.jpg"
+    , caption = "Evening"
+    , liked  = False
+    }
 
-viewDetailedPhoto : String -> String -> Html msg
-viewDetailedPhoto url caption =
+viewDetailedPhoto : { url : String, caption : String, liked : Bool } -> Html Msg
+viewDetailedPhoto model =
+    let 
+        buttonClass = 
+            if model.liked then 
+                "fa-heart"
+            else 
+                "fa-heart-o"
+        msg: Msg
+        msg = 
+            if model.liked then
+                Unlike
+            else
+                Like 
+    in 
     div [ class "detailed-photo" ]
-        [ img [ src url ] []
+        [ img [ src model.url ] []
         , div [ class "photo-info" ]
-            [ h2 [ class "caption", align "center" ] [ text caption ] ]
+            [ div [class "like-button"]
+                [ i 
+                    [class "fa fa-2x" 
+                    , class buttonClass 
+                    , onClick msg 
+                    ]
+                     []
+                ]
+            , h2 [ class "caption", align "center" ] [ text model.caption ] 
+            ]
         ]
 
-main : Html msg -- Html (type) msg (type variable)
-main = 
+view : { url : String, caption : String, liked : Bool } -> Html Msg -- Html (type) msg (type variable)
+view model = 
     div []
         [ div [class "header"]
             [ h1 [] [text "Picshare"]]
         , div [ class "content-flow" ]
-            [ div [ class "detailed-photo" ]
-                [ viewDetailedPhoto (baseUrl ++ "1.jpg") "Surfing"
-                , viewDetailedPhoto (baseUrl ++ "2.jpg") "The Fox"
-                , viewDetailedPhoto (baseUrl ++ "3.jpg") "Evening"
-                ]
+            [ viewDetailedPhoto model
             ]
         ]
+type Msg =
+    Like | Unlike
 
+update : Msg 
+        -> { url : String, caption : String, liked : Bool} 
+        -> { url : String, caption : String, liked : Bool } 
+update msg model = 
+    case msg of 
+        Like -> 
+            { model | liked = True }
+        Unlike ->
+            { model | liked = False }
+
+
+main : Program Never { url : String, caption : String, liked : Bool } Msg
+main = 
+    Html.beginnerProgram 
+        { model = initialModel 
+        , view = view
+        , update = update 
+        }
